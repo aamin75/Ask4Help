8dfbf8d0330f4ea6257d5d3d9de7055e312c0f8d
Ammar Amin <ammar.amin1975@gmail.com>
2023-01-22 05:06:24 +0100

    add social media icons to the footer

--- Frontend/src/components/Footer/Footer.js
+++ Frontend/src/components/Footer/Footer.js
@@ -14,14 +14,14 @@ function Footer (){
                 </div>
                 <div className="contact_us">
                     <h4>Contact us:</h4>
-                    <h5>Email:</h5>
-                    <h5>Phone:</h5>
+                    <h5>Email: ask@A4H.com</h5>
+                    <h5>Phone: +46 (0) 111 1111 11</h5>
                 </div>
                 <div className="follow_us">
                     <h4>Follow us:</h4>
-                    <h5><a href="https://www.twitter.com">Twitter</a></h5>
-                    <h5><a href="https://www.facebook.com">Facebook</a></h5>
-                    <h5><a href="https://www.instagram.com">Instagram</a></h5>
+                    <h5><a href="https://www.twitter.com">Twitter  <i className="fa fa-twitter" ></i></a></h5>
+                    <h5><a href="https://www.facebook.com">Facebook  <i className="fa fa-facebook-f" ></i></a></h5>
+                    <h5><a href="https://www.instagram.com">Instagram  <i className="fa fa-instagram" ></i></a></h5>
                 </div>
         </div>
     )
