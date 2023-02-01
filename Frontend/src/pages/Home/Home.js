44c752052ea93073355c3ba38709ef62f4b3f476
KarimIsaac <karim.isaac@lerniastudent.se>
2023-01-31 19:03:22 -0800

    Fix posts

--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -5,7 +5,7 @@ import './Home.css';
 
 
 function  Home(isAuth)  {
-  const [postLists, setPostList]= useState([]);
+  const [postLists, setPostList]= useState([{title:"test1", post:"this is a post", email:"user@gmail.com"},{title:"test5", post:"this is a post3", email:"user3@gmail.com",}, {title:"this title", post:"this is a post test", email:"admin@gmail.com"}]);
   const postsCollection = collection(db, "post");
   useEffect(()=>{
     const getPost = async () => {
@@ -15,16 +15,28 @@ function  Home(isAuth)  {
     getPost();
   });
 
-  return <div className="home-container">{postLists.map(( post)=>{
-    return (
-      <>
-        <div className="post">{post.title}</div>
-        <h3>{post.post}</h3>
-        <h3>{post.email}</h3>
-      </>
-    )
-  })}</div>
-     
+  return (
+    <div className="home-container">
+      <div className="box-post">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+              <div className="title">
+                <h1> {post.title}</h1>
+              </div>
+              
+           
+            <div className="postTextContainer"> {post.postText} </div>
+            <h3>{post.post}</h3>
+            <h3>{post.email}</h3>
+            
+          </div>
+          
+        );
+      })}
+      </div>
+    </div>
+   )   
 }
 
 export default Home;
